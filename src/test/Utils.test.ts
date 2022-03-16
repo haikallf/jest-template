f62edafb9fb60e51ac653302acc5433df9867753
haikallf <haikallzrd@gmail.com>
2022-03-17 00:00:53 +0700

    jest template

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,8 @@
+import { Utils } from "../app/Utils";
+
+describe("utils est suite", () => {
+  test("first test", () => {
+    const abc = Utils.toUpperCase("abc");
+    console.log("test");
+  });
+});
